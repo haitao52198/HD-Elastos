e2f34915d8c9c29647f898a46edd2c700097a58e
XilongPei <pei.xilong@kortide.com>
2014-11-27 09:41:16 +0800

    add omap4 support code, now the built-image could be run on pandaboard, but stop after "Jumping to kernel-image entry point..."

--- goose/libs/libplatsupport/src/plat/omap4/serial.c
+++ goose/libs/libplatsupport/src/plat/omap4/serial.c
@@ -1,4 +1,3 @@
-#if 0
 /*
  * Copyright 2014, NICTA
  *
@@ -8,50 +7,101 @@
  *
  * @TAG(NICTA_BSD)
  */
-/*
- * Provide serial UART glue to hardware. This file is not used on debug
- * kernels, as we use a seL4 debug syscall to print to the serial console.
- */
 
-#include <sel4/sel4.h>
-#include "../../plat_internal.h"
-#include <stdio.h>
+#include "serial.h"
+#include <string.h>
+#include <stdlib.h>
 
-#define UART_PADDR     0x48020000
-#define UART_REG(x)    ((volatile seL4_Word *)(uart_page + (x)))
-#define UART_SR1_TRDY  5
-#define UTXD           0x00
-#define USR1           0x14
+#define IMXUART_DLL             0x000
+#define IMXUART_RHR             0x000
+#define IMXUART_THR             0x000
 
-static void* uart_page;
+#define IMXUART_LSR             0x014
+#define IMXUART_LSR_RXFIFIOE    (1<<0)
+#define IMXUART_LSR_RXOE        (1<<1)
+#define IMXUART_LSR_RXPE        (1<<2)
+#define IMXUART_LSR_RXFE        (1<<3)
+#define IMXUART_LSR_RXBI        (1<<4)
+#define IMXUART_LSR_TXFIFOE     (1<<5)
+#define IMXUART_LSR_TXSRE       (1<<6)
+#define IMXUART_LSR_RXFIFOSTS   (1<<7)
 
-void
-__plat_serial_input_init_IRQ(void)
+#define REG_PTR(base, offset)  ((volatile uint32_t *)((char*)(base) + (offset)))
+
+static int uart_getchar(ps_chardevice_t* d)
 {
-}
-
-void
-__plat_serial_init(void)
-{
-    uart_page = __map_device_page(UART_PADDR, 12);
-}
-
-void
-__plat_putchar(int c)
-{
-    /* Wait for serial to become ready. */
-    while (!(*UART_REG(USR1) & BIT(UART_SR1_TRDY)));
-
-    /* Write out the next character. */
-    *UART_REG(UTXD) = c;
-    if (c == '\n') {
-        __plat_putchar('\r');
+    if (*REG_PTR(d->vaddr, IMXUART_LSR) & IMXUART_LSR_RXFIFIOE) {
+        return *REG_PTR(d->vaddr, IMXUART_RHR);
+    } else {
+        return -1;
     }
 }
 
-int
-__plat_getchar(void)
+static int uart_putchar(ps_chardevice_t* d, int c)
 {
-    return EOF;
+    if (*REG_PTR(d->vaddr, IMXUART_LSR) & IMXUART_LSR_TXFIFOE) {
+        *REG_PTR(d->vaddr, IMXUART_THR) = c;
+        return c;
+    } else {
+        return -1;
+    }
 }
-#endif
+
+static void uart_handle_irq(ps_chardevice_t* d)
+{
+    /* TODO */
+}
+
+static ssize_t
+uart_write(ps_chardevice_t* d, const void* vdata, size_t count, chardev_callback_t rcb UNUSED, void* token UNUSED)
+{
+    const char* data = (const char*)vdata;
+    int i;
+    for (i = 0; i < count; i++) {
+        if (uart_putchar(d, *data++) < 0) {
+            return i;
+        }
+    }
+    return count;
+}
+
+static ssize_t
+uart_read(ps_chardevice_t* d, void* vdata, size_t count, chardev_callback_t rcb UNUSED, void* token UNUSED)
+{
+    char* data;
+    int ret;
+    int i;
+    data = (char*)vdata;
+    for (i = 0; i < count; i++) {
+        ret = uart_getchar(d);
+        if (ret != EOF) {
+            *data++ = ret;
+        } else {
+            return i;
+        }
+    }
+    return count;
+}
+
+
+
+int
+uart_init(const struct dev_defn* defn,
+          const ps_io_ops_t* ops,
+          ps_chardevice_t* dev)
+{
+    void* vaddr = chardev_map(defn, ops);
+    memset(dev, 0, sizeof(*dev));
+    if (vaddr == NULL) {
+        return -1;
+    }
+    dev->id         = defn->id;
+    dev->vaddr      = vaddr;
+    dev->read       = &uart_read;
+    dev->write      = &uart_write;
+    dev->handle_irq = &uart_handle_irq;
+    dev->irqs       = defn->irqs;
+    dev->ioops      = *ops;
+
+    return 0;
+}
