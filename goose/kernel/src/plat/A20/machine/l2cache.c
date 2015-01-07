a7e3797d330b142e6972a74b9e024e9d05e0ed29
jingxuanzhang <jingxuan.n.zhang@gmail.com>
2015-01-07 14:24:01 +0800

    some support for A20 (not complete).

--- goose/kernel/src/plat/A20/machine/l2cache.c
+++ goose/kernel/src/plat/A20/machine/l2cache.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014, General Dynamics C4 Systems
+ * Copyright 2015, Tongji Operating System Group & elastos.org
  *
  * This software may be distributed and modified according to the terms of
  * the GNU General Public License version 2. Note that NO WARRANTY is provided.
