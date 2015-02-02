251b2a2929cf19af246fbf0e3ba09481130eef50
XilongPei <pei.xilong@kortide.com>
2015-02-02 14:15:18 +0800

    Add GetAddress arch-independent #defines. & Add const qualifier to thread_name in sel4utils_print_fault_message

--- zener/libs/libsel4utils/include/sel4utils/mapping.h
+++ zener/libs/libsel4utils/include/sel4utils/mapping.h
@@ -29,6 +29,8 @@ static int UNUSED sel4_supported_page_sizes[] = {12, 16, 20, 24};
 
 #define seL4_ARCH_Page_Map             seL4_ARM_Page_Map
 #define seL4_ARCH_Page_Unmap           seL4_ARM_Page_Unmap
+#define seL4_ARCH_Page_GetAddress      seL4_ARM_Page_GetAddress
+#define seL4_ARCH_Page_GetAddress_t    seL4_ARM_Page_GetAddress_t
 #define seL4_ARCH_PageTable_Map        seL4_ARM_PageTable_Map
 #define seL4_ARCH_ASIDPool_Assign      seL4_ARM_ASIDPool_Assign
 #define seL4_ARCH_PageTableObject      seL4_ARM_PageTableObject
@@ -36,6 +38,8 @@ static int UNUSED sel4_supported_page_sizes[] = {12, 16, 20, 24};
 #define seL4_ARCH_Default_VMAttributes seL4_ARM_Default_VMAttributes
 #define seL4_ARCH_VMAttributes         seL4_ARM_VMAttributes
 #define seL4_ARCH_4KPage               seL4_ARM_SmallPageObject
+/* Remap does not exist on all kernels */
+#define seL4_ARCH_Page_Remap           seL4_ARM_Page_Remap
 
 #elif defined(CONFIG_X86_64)
 
@@ -44,6 +48,8 @@ static int UNUSED sel4_supported_page_sizes[] = {12, 21};
 
 #define seL4_ARCH_Page_Unmap                    seL4_X64_Page_Unmap
 #define seL4_ARCH_Page_Map                      seL4_X64_Page_Map
+#define seL4_ARCH_Page_GetAddress               seL4_X64_Page_GetAddress
+#define seL4_ARCH_Page_GetAddress_t             seL4_X64_Page_GetAddress_t
 #define seL4_ARCH_PageTable_Map                 seL4_X64_PageTable_Map
 #define seL4_ARCH_PageDirectory_Map             seL4_X64_PageDirectory_Map
 #define seL4_ARCH_PageDirectoryPointerTable_Map seL4_X64_PageDirectoryPointerTable_Map
@@ -59,6 +65,8 @@ static int UNUSED sel4_supported_page_sizes[] = {12, 22};
 
 #define seL4_ARCH_Page_Map             seL4_IA32_Page_Map
 #define seL4_ARCH_Page_Unmap           seL4_IA32_Page_Unmap
+#define seL4_ARCH_Page_GetAddress      seL4_IA32_Page_GetAddress
+#define seL4_ARCH_Page_GetAddress_t    seL4_IA32_Page_GetAddress_t
 #define seL4_ARCH_PageTable_Map        seL4_IA32_PageTable_Map
 #define seL4_ARCH_ASIDPool_Assign      seL4_IA32_ASIDPool_Assign
 #define seL4_ARCH_PageTableObject      seL4_IA32_PageTableObject
@@ -67,6 +75,8 @@ static int UNUSED sel4_supported_page_sizes[] = {12, 22};
 #define seL4_ARCH_VMAttributes         seL4_IA32_VMAttributes
 #define seL4_ARCH_4KPage               seL4_IA32_4K
 #define seL4_ARCH_Uncached_VMAttributes 0
+/* Remap does not exist on all kernels */
+#define seL4_ARCH_Page_Remap           seL4_IA32_Page_Remap
 
 #endif /* CONFIG_ARCH_IA32 */
 
